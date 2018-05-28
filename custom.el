4c08847a182d2f3e286e6b52a0a41a73cfcdbece
Greap <purple_greap@hotmail.com>
2018-05-28 18:54:36 +0545

    Yanked the custom f. to custom.el file

---
+++ custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (klere)))
+ '(custom-safe-themes
+   (quote
+    ("f41ecd2c34a9347aeec0a187a87f9668fa8efb843b2606b6d5d92a653abe2439" "476bac2b4e56253a30d5b20559f94c047ea7548363b7607b3e456b26685dc34c" default)))
+ '(package-selected-packages
+   (quote
+    (mark-multiple helm-tramp avy xcscope helm company rainbow xscope use-package rainbow-mode powerline org klere-theme helm-cscope elpy))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
