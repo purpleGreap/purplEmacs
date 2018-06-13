db565c54e0c3c0fd944f742507703fe53f923394
Greap <purple_greap@hotmail.com>
2018-06-13 12:53:28 +0545

    Cleaned custom.el file.

---
+++ custom.el
@@ -0,0 +1,23 @@
+;;--------------------
+;;VARIABLES
+;;--------------------
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (flycheck rainbow-mode powerline org-bullets mark-multiple helm-cscope helm company avy use-package))))
+
+;;--------------------
+;;FACES
+;;--------------------
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
